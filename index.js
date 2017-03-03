76574218cf9b2317ac8fc172fedece7f6a3bb565
Mahadev Vyavahare <mahadev@thoughtworks.com>
2017-03-04 00:07:07 +0530

    Add log directory

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var express = require('express')
+var fs = require('fs')
+var Log = require('log')
 var app = express()
+var readConfig = require('read-config'),
+    config = readConfig('config/server.json');
+
+var log = new Log('info',fs.createWriteStream(config["LOG_DIR"],{flags:"a"}));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+	log.info('Request on /');
+    res.send('Hello World!')
 })
 
-app.listen(3000, function () {
+app.listen(config["PORT"], function () {
   console.log('Example app listening on port 3000!')
 })
